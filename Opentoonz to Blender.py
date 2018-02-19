072812c98b6ebe1408cdc84dc0b31a4a37f95cb3
AdamEarle <MrAdamEarle@gmail.com>
2018-02-19 19:25:03 +1100

    Setup
    Setting up a basic  overview

---
+++ Opentoonz to Blender.py
@@ -0,0 +1,10 @@
+'''
+OVERVIEW:
+    1. Switching out camera Data
+    2. OpenToonz column data as image planes sequence files
+            Working with vector and grease pencil
+    3. Check to see if its possible bring across some compisting features of OT to Blender and vice versa
+
+
+
+'''
